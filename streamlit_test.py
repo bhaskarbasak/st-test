02acae0bc700e9cc2ed4f4fefffd5e2b60ef446b
bhaskarbasak <37381470+bhaskarbasak@users.noreply.github.com>
2023-08-02 19:30:36 +0530

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -25,7 +25,7 @@ uploaded_file_template = st.file_uploader("Choose the template file")
 if uploaded_file_template is not None:
     template = uploaded_file_template
 
-zipObj = ZipFile("sample_new_options.zip", "w")
+zipObj = ZipFile("sample.zip", "w")
 
 if option =='template1':
     for index, row in df.iterrows():
@@ -56,35 +56,14 @@ if option =='template2':
         zipObj.write(f"generated_doc2_{index}.docx")
     zipObj.close()
 
-ZipfileDotZip = "sample_new_options"
+ZipfileDotZip = "sample.zip"
 
 with open(ZipfileDotZip, "rb") as f:
     bytes = f.read()
     b64 = base64.b64encode(bytes).decode()
     href = f"<a href=\"data:file/zip;base64,{b64}\" download='{ZipfileDotZip}.zip'>\
-        Click last model weights\
+        Download letters\
     </a>"
+    
 st.sidebar.markdown(href, unsafe_allow_html=True)
 
-"""if uploaded_file is not None:
-    bytes_data = uploaded_file.getvalue()
-    data = uploaded_file.getvalue().decode('utf-8').splitlines()         
-    st.session_state["preview"] = ''
-    for i in range(0, min(5, len(data))):
-        st.session_state["preview"] += data[i]
-preview = st.text_area("CSV Preview", "", height=150, key="preview")
-upload_state = st.text_area("Upload State", "", key="upload_state")
-def upload():
-    if uploaded_file is None:
-        st.session_state["upload_state"] = "Upload a file first!"
-    else:
-        data = uploaded_file.getvalue().decode('utf-8')
-        parent_path = pathlib.Path(__file__).parent.parent.resolve()           
-        save_path = os.path.join(parent_path, "data")
-        complete_name = os.path.join(save_path, uploaded_file.name)
-        destination_file = open(complete_name, "w")
-        destination_file.write(data)
-        destination_file.close()
-        st.session_state["upload_state"] = "Saved " + complete_name + " successfully!"
-st.button("Upload file to Sandbox", on_click=upload)
-"""
